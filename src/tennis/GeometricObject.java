678b1e6eb3c42f9fcd2e8d246340a8031e9a353a
Somi <Somi@DESKTOP-A9ECUTM.local>
2017-05-01 15:12:15 +0200

    Finished GeometricObject classes, needs review

--- src/tennis/GeometricObject.java
+++ src/tennis/GeometricObject.java
@@ -2,27 +2,42 @@ package tennis;
 
 public abstract class GeometricObject {
 	
-private Integer[] colour = new Integer[3];
+private Integer[] colourRGB = new Integer[3];
 private Float[] coordinates = new Float[2];
 private Float direction = null;
 private Float velocity = null;
 
-abstract void time();
-public GeometricObject(Integer Colour[],Float[] Coord) {
+public GeometricObject(Integer Colour[],Float Coord[]) {
 	if(Colour.length==3)
-		colour=Colour;
+		colourRGB=Colour;
 	if(Coord.length==3)
 		coordinates=Coord;
 	velocity=(float) 0;
 	direction=(float) 0;
 }
 
-void setDirection(Float Dir){
-	direction=Dir;
+public Integer[] getColour() {
+	return colourRGB;
+}
+public Float[] getCoordinates() {
+	return coordinates;
+}
+public Float getDirection() {
+	return direction;
+}
+public Float getVelocity() {
+	return velocity;
 }
 
-void setVelocity(Float Vel){
+public void setCoordinates(Float[] coordinates) {
+	this.coordinates = coordinates;
+}
+public void setDirection(Float Dir){
+	direction=Dir;
+}
+public void setVelocity(Float Vel){
 	velocity=Vel;
 }
 
+abstract void time();
 }
--- src/tennis/Racket.java
+++ src/tennis/Racket.java
@@ -1,5 +1,24 @@
 package tennis;
 
-public class Racket {
+public class Racket extends GeometricObject{
+	
+private final Float width;
+private final Float height;
 
+public Racket(Integer colour[],Float coordinates[], Float wid, Float hei){
+	
+	super(colour,coordinates);
+	width=wid;
+	height=hei;
+	
+}
+
+public Float getHeight() {
+	return height;
+}
+public Float getWidth() {
+	return width;
+}
+
+void time(){}
 }
